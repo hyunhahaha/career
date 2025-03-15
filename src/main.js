053310501d8b9e58c885b1af36e939b095a3a30f
kindtiger <seonbeom2@gmail.com>
2025-03-15 14:56:42 +0900

    7차 컨펌

--- src/main.js
+++ src/main.js
@@ -1,15 +1,41 @@
 import "../lib/smooth";
 import "./styles/style.css";
+import 'swiper/css/bundle';
 import { markers } from "../lib/smooth";
 
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Swiper from 'swiper/bundle';
 // import { Draggable } from "gsap/Draggable";
 // import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+
+// import Swiper bundle with all modules installed
+
+// import styles bundle
 
 gsap.registerPlugin(ScrollTrigger);
 // ,Draggable,MotionPathPlugin
 
+const swiper = new Swiper('.swiper1', {
+  // Optional parameters
+  // loop: true,
+
+  // // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  // // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
 
 
 
