c1276fd6321ff98e84b392bb2e2b88d6ce4c3fbb
hyunhahaha <pryu3633@naver.com>
2025-05-12 22:38:09 +0900

    0512

--- src/main.js
+++ src/main.js
@@ -97,7 +97,20 @@ const swiper4 = new Swiper(".swiper4", {
 
 const detail_1 = document.querySelectorAll(".detail_1_line > div");
 const detail_2 = document.querySelectorAll(".detail_2_line > div");
+const detail_3 = document.querySelectorAll(".detail_3_line > div");
+const detail_4 = document.querySelectorAll(".detail_4_line > div");
+const event_1 = document.querySelectorAll(".event_1_line > div");
+const event_2 = document.querySelectorAll(".event_2_line > div");
+const banner_1 = document.querySelectorAll(".banner_1_line > div");
+const banner_2 = document.querySelectorAll(".banner_2_line > div");
+const ad_1 = document.querySelectorAll(".ad_1_line > div");
+const ad_2 = document.querySelectorAll(".ad_2_line > div");
+const ad_3 = document.querySelectorAll(".ad_3_line > div");
 const fashion_1 = document.querySelectorAll(".fashion_1_line > div");
+const fashion_2 = document.querySelectorAll(".fashion_2_line > div");
+const fashion_3 = document.querySelectorAll(".fashion_3_line > div");
+const fashion_4 = document.querySelectorAll(".fashion_4_line > div");
+const fashion_5 = document.querySelectorAll(".fashion_5_line > div");
 const menuList = document.querySelectorAll(".category > div");
 const pages = document.querySelectorAll(".pages");
 
@@ -146,13 +159,263 @@ detail_2.forEach((item, i) => {
 });
 
 
+detail_3.forEach((item, i) => {
+  item.addEventListener("click", (e) => {
+
+    e.stopPropagation();
+    
+    switch (i) {
+      case 0: gsap.to(".pop9", { autoAlpha: 1 }); break;
+      case 1: gsap.to(".pop10", { autoAlpha: 1 }); break;
+      case 2: gsap.to(".pop11", { autoAlpha: 1 }); break;
+      case 3: gsap.to(".pop12", { autoAlpha: 1 }); break;
+    }
+    
+    // swiper1.slideTo(i);
+  });
+});
+
+
+detail_4.forEach((item, i) => {
+  item.addEventListener("click", (e) => {
+
+    e.stopPropagation();
+    
+    switch (i) {
+      case 0: gsap.to(".pop13", { autoAlpha: 1 }); break;
+
+    }
+    
+    // swiper1.slideTo(i);
+  });
+});
+
+
+
+
+
+
+
+
+
+event_1.forEach((item, i) => {
+  item.addEventListener("click", (e) => {
+
+    e.stopPropagation();
+    
+    switch (i) {
+      case 0: gsap.to(".pop14", { autoAlpha: 1 }); break;
+      case 1: gsap.to(".pop15", { autoAlpha: 1 }); break;
+      case 2: gsap.to(".pop16", { autoAlpha: 1 }); break;
+      case 3: gsap.to(".pop17", { autoAlpha: 1 }); break;
+
+    }
+    
+    // swiper1.slideTo(i);
+  });
+});
+
+
+event_2.forEach((item, i) => {
+  item.addEventListener("click", (e) => {
+
+    e.stopPropagation();
+    
+    switch (i) {
+      case 0: gsap.to(".pop18", { autoAlpha: 1 }); break;
+      case 1: gsap.to(".pop19", { autoAlpha: 1 }); break;
+      case 2: gsap.to(".pop20", { autoAlpha: 1 }); break;
+
+    }
+    
+    // swiper1.slideTo(i);
+  });
+});
+
+
+
+
+
+
+
+banner_1.forEach((item, i) => {
+  item.addEventListener("click", (e) => {
+
+    e.stopPropagation();
+    
+    switch (i) {
+      case 0: gsap.to(".pop21", { autoAlpha: 1 }); break;
+      case 1: gsap.to(".pop22", { autoAlpha: 1 }); break;
+      case 2: gsap.to(".pop23", { autoAlpha: 1 }); break;
+      case 3: gsap.to(".pop24", { autoAlpha: 1 }); break;
+
+    }
+    
+    // swiper1.slideTo(i);
+  });
+});
+
+
+banner_2.forEach((item, i) => {
+  item.addEventListener("click", (e) => {
+
+    e.stopPropagation();
+    
+    switch (i) {
+      case 0: gsap.to(".pop25", { autoAlpha: 1 }); break;
+      case 1: gsap.to(".pop26", { autoAlpha: 1 }); break;
+      case 2: gsap.to(".pop27", { autoAlpha: 1 }); break;
+      case 3: gsap.to(".pop28", { autoAlpha: 1 }); break;
+
+    }
+    
+    // swiper1.slideTo(i);
+  });
+});
+
+
+
+
+
+
+
+
+ad_1.forEach((item, i) => {
+  item.addEventListener("click", (e) => {
+
+    e.stopPropagation();
+    
+    switch (i) {
+      case 0: gsap.to(".pop29", { autoAlpha: 1 }); break;
+      case 1: gsap.to(".pop30", { autoAlpha: 1 }); break;
+      case 2: gsap.to(".pop31", { autoAlpha: 1 }); break;
+      case 3: gsap.to(".pop32", { autoAlpha: 1 }); break;
+
+    }
+    
+    // swiper1.slideTo(i);
+  });
+});
+
+
+ad_2.forEach((item, i) => {
+  item.addEventListener("click", (e) => {
+
+    e.stopPropagation();
+    
+    switch (i) {
+      case 0: gsap.to(".pop33", { autoAlpha: 1 }); break;
+      case 1: gsap.to(".pop34", { autoAlpha: 1 }); break;
+      case 2: gsap.to(".pop35", { autoAlpha: 1 }); break;
+      case 3: gsap.to(".pop36", { autoAlpha: 1 }); break;
+
+    }
+    
+    // swiper1.slideTo(i);
+  });
+});
+
+
+ad_3.forEach((item, i) => {
+  item.addEventListener("click", (e) => {
+
+    e.stopPropagation();
+    
+    switch (i) {
+      case 0: gsap.to(".pop37", { autoAlpha: 1 }); break;
+      case 1: gsap.to(".pop38", { autoAlpha: 1 }); break;
+
+    }
+    
+    // swiper1.slideTo(i);
+  });
+});
+
+
+
+
+
+
+
+
 fashion_1.forEach((item, i) => {
   item.addEventListener("click", (e) => {
 
     e.stopPropagation();
     
     switch (i) {
-      case 0: gsap.to(".pop0", { autoAlpha: 1 }); break;
+      case 0: gsap.to(".pop39", { autoAlpha: 1 }); break;
+      case 1: gsap.to(".pop40", { autoAlpha: 1 }); break;
+      case 2: gsap.to(".pop41", { autoAlpha: 1 }); break;
+      case 3: gsap.to(".pop42", { autoAlpha: 1 }); break;
+    }
+    
+    // swiper1.slideTo(i);
+  });
+});
+
+
+fashion_2.forEach((item, i) => {
+  item.addEventListener("click", (e) => {
+
+    e.stopPropagation();
+    
+    switch (i) {
+      case 0: gsap.to(".pop43", { autoAlpha: 1 }); break;
+      case 1: gsap.to(".pop44", { autoAlpha: 1 }); break;
+      case 2: gsap.to(".pop45", { autoAlpha: 1 }); break;
+      case 3: gsap.to(".pop46", { autoAlpha: 1 }); break;
+    }
+    
+    // swiper1.slideTo(i);
+  });
+});
+
+
+fashion_3.forEach((item, i) => {
+  item.addEventListener("click", (e) => {
+
+    e.stopPropagation();
+    
+    switch (i) {
+      case 0: gsap.to(".pop47", { autoAlpha: 1 }); break;
+      case 1: gsap.to(".pop48", { autoAlpha: 1 }); break;
+      case 2: gsap.to(".pop49", { autoAlpha: 1 }); break;
+      case 3: gsap.to(".pop50", { autoAlpha: 1 }); break;
+    }
+    
+    // swiper1.slideTo(i);
+  });
+});
+
+
+fashion_4.forEach((item, i) => {
+  item.addEventListener("click", (e) => {
+
+    e.stopPropagation();
+    
+    switch (i) {
+      case 0: gsap.to(".pop51", { autoAlpha: 1 }); break;
+      case 1: gsap.to(".pop52", { autoAlpha: 1 }); break;
+      case 2: gsap.to(".pop53", { autoAlpha: 1 }); break;
+      case 3: gsap.to(".pop54", { autoAlpha: 1 }); break;
+    }
+    
+    // swiper1.slideTo(i);
+  });
+});
+
+
+fashion_5.forEach((item, i) => {
+  item.addEventListener("click", (e) => {
+
+    e.stopPropagation();
+    
+    switch (i) {
+      case 0: gsap.to(".pop55", { autoAlpha: 1 }); break;
+      case 1: gsap.to(".pop56", { autoAlpha: 1 }); break;
+      case 2: gsap.to(".pop57", { autoAlpha: 1 }); break;
+      case 3: gsap.to(".pop58", { autoAlpha: 1 }); break;
     }
     
     // swiper1.slideTo(i);
